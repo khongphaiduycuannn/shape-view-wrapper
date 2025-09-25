7e5680ae135e46b703bd7e7653422894a1f4fd95
chuaterungxanh <quanndm@infinitytech.global>
2025-09-25 18:09:11 +0700

    format code

--- permission/src/main/java/com/ndmquan/permission/utils/PermissionExtension.kt
+++ permission/src/main/java/com/ndmquan/permission/utils/PermissionExtension.kt
@@ -38,38 +38,30 @@ fun AppCompatActivity.openSettingPermission(permission: String? = null) {
 
 private fun AppCompatActivity.createSettingIntent(permission: String?): Intent {
     return when (permission) {
-        Manifest.permission.SYSTEM_ALERT_WINDOW -> {
-            Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION).apply {
-                data = Uri.fromParts("package", packageName, null)
-            }
+        Manifest.permission.SYSTEM_ALERT_WINDOW -> Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION).apply {
+            data = Uri.fromParts("package", packageName, null)
         }
 
-        Manifest.permission.POST_NOTIFICATIONS -> {
-            Intent().apply {
-                when {
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
-                        action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
-                        putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-                    }
+        Manifest.permission.POST_NOTIFICATIONS -> Intent().apply {
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+                    action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                    putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                }
 
-                    else -> {
-                        action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                        data = Uri.fromParts("package", packageName, null)
-                    }
+                else -> {
+                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    data = Uri.fromParts("package", packageName, null)
                 }
             }
         }
 
-        Manifest.permission.WRITE_SETTINGS -> {
-            Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS).apply {
-                data = Uri.fromParts("package", packageName, null)
-            }
+        Manifest.permission.WRITE_SETTINGS -> Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
         }
 
-        else -> {
-            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                data = Uri.fromParts("package", packageName, null)
-            }
+        else -> Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
         }
     }
 }
